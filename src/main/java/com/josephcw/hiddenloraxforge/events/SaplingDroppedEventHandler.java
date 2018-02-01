f8d929bf9bbb210a611990fa7872818ce896998b
Joseph Watts <josephcw@live.com>
2018-02-01 12:44:44 -0600

    Verify that the sapling is on a valid block

--- src/main/java/com/josephcw/hiddenloraxforge/events/SaplingDroppedEventHandler.java
+++ src/main/java/com/josephcw/hiddenloraxforge/events/SaplingDroppedEventHandler.java
@@ -1,36 +1,60 @@
 package com.josephcw.hiddenloraxforge.events;
 
+import java.util.HashSet;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3i;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.item.ItemExpireEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @EventBusSubscriber
 public class SaplingDroppedEventHandler {
+	
+	public static HashSet<String> allowedSet;
+	
 	@SubscribeEvent
 	public static void itemExpires(ItemExpireEvent e) {
-		
-		
-		EntityItem expiringItem = e.getEntityItem();
-		if (expiringItem.onGround) {
-			
-			ItemStack droppedItemStack = expiringItem.getItem();
-			Item singleItem = droppedItemStack.getItem();
-			String itemName2 = singleItem.getRegistryName().toString();
-			String itemName3 = droppedItemStack.getDisplayName();
-			String itemName4 = droppedItemStack.getUnlocalizedName();
-			System.out.println("2: " + itemName2);
-			System.out.println("3: " + itemName3);
-			System.out.println("4: " + itemName4);
-			
-			// item.unlocalized = item.sapling
-			// item.registryName.toString = minecraft:sapling
-			// itemStack.getDisplaynaem = oaksapling
-			// itemStack.getUnlocalizedName = tile.sapling.oak
+		ItemStack itemStack = getItemStack(e);
+		Item singleItem = itemStack.getItem();
+		if (isAllowedItem(singleItem)) {
+			if (isPlantableLocation(itemStack, e)) {
+				//plantItem(singleItem);
+			}
 		}
 	}
 	
+	private static ItemStack getItemStack(ItemExpireEvent e) {
+		EntityItem expiringItem = e.getEntityItem();
+		ItemStack droppedItemStack = expiringItem.getItem();
+		return droppedItemStack;
+	}
+	
+	private static boolean isAllowedItem(Item item) {
+		String registryName = item.getRegistryName().toString();
+		return allowedSet.contains(registryName);
+	}
+	
+	private static boolean isPlantableLocation(ItemStack itemStack, ItemExpireEvent e) {
+		// Get the xyz position of our dropped item stack
+		Entity entityItemStack = e.getEntity();
+		World eventWorld = e.getEntity().getEntityWorld();
+		BlockPos itemPosition = entityItemStack.getPosition();
+		// Subtract y by one to get the position of the block the item is on
+		BlockPos onePosUnder = itemPosition.subtract(new Vec3i(0, 1, 0));
+		// get the block and its name
+		Block blockOn = eventWorld.getBlockState(onePosUnder).getBlock();
+		String blockOnRegName = blockOn.getRegistryName().toString();
+		if (blockOnRegName.equals("minecraft:grass") 
+				|| blockOnRegName.equals("minecraft:dirt")) {
+			return true;
+		}
+		return false;
+	}
 }
--- src/main/java/com/josephcw/hiddenloraxforge/proxy/CommonProxy.java
+++ src/main/java/com/josephcw/hiddenloraxforge/proxy/CommonProxy.java
@@ -1,5 +1,9 @@
 package com.josephcw.hiddenloraxforge.proxy;
 
+import java.util.HashSet;
+
+import com.josephcw.hiddenloraxforge.events.SaplingDroppedEventHandler;
+
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -15,6 +19,8 @@ public class CommonProxy {
     }
 
     public void postInit(FMLPostInitializationEvent e) {
-
+    	SaplingDroppedEventHandler.allowedSet = new HashSet<String>();
+    	
+    	SaplingDroppedEventHandler.allowedSet.add("minecraft:sapling");
     }
 }
